23a5056cda3a83fa3db3ef977a3b74e16a825fb5
Kenji Inoue <Kenji Inoue>
2016-04-20 12:56:13 -0400

    sd

--- Julia_sim/julia.cpp
+++ Julia_sim/julia.cpp
@@ -9,3 +9,39 @@ Julia::Julia(float a,float b,float bailout,float iter)
 
 
 }
+void Julia::process(){
+    int x,y;
+    QRgb pix;
+    for(x = 0; x < 640; x++){
+        for(y = 0; y < 480; y++){
+            pix = calcPixel(x,y);
+            this->im.setPixel(x,y,pix);
+        }
+    }
+    return;
+}
+
+QRgb Julia::calcPixel(int x, int y){
+    int i = 0;
+    float zreal = x;
+    float zim = y;
+    int numit;
+    //float zreal = ((float)(pow(x,2) - pow(y,2)))+this->a;
+    //float zim = ((float)(2*x*y) + this->b);
+
+    while(i < this->iter){
+
+        zreal = ((float)(pow(zreal,2) - pow(zim,2)))+this->a;
+        zim = ((float)(2*zreal*zim) + this->b);
+        if (this->magnitude(zreal,zim) >= bailout){
+            numit = i;
+            i = this->iter;
+        }
+
+    }
+    return 0;
+}
+
+float Julia::magnitude(float zreal, float zim){
+    return (pow((pow(zreal,2)+pow(zim,2)),.5));
+}
